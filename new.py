b5f7d362d7202bc77d720a21cb3a68e93acf74d0
Muhammad Soman Shabbir Abb <mssabbasi306@gmail.com>
2025-08-03 13:04:39 +0500

    steps for pushing new files on repository

--- new.py
+++ new.py
@@ -7,3 +7,20 @@
 # git remote add origin https://github.com/yourusername/flask-rest-api.git
 # git branch -M main
 # git push -u origin main
+
+"""
+Whenever you add or change files, follow these:
+git add .
+
+If you want to add a specific file only:
+git add your_file.py
+git commit -m "Add feature XYZ or update ABC file"
+git push
+You only need git push, because you've already set the upstream with -u origin main earlier.
+
+
+git status   # Shows what changed and what will be committed
+git diff     # Shows actual code differences
+
+
+"""
