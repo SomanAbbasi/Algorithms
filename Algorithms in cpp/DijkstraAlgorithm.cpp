b6e6a3174a548ee59e9dfc57acea3b4bcb16adb0
Soman Abbasi <mssabbasi306@gmail.com>
2025-12-06 10:55:57 +0500

    Dijkstra using maxHeap

--- Algorithms in cpp/DijkstraAlgorithm.cpp
+++ Algorithms in cpp/DijkstraAlgorithm.cpp
@@ -83,5 +83,61 @@ int main()
 }
 
 
+/*
+
+class Solution
+{
+public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src)
+    {
+
+        vector<vector<pair<int, int>>> graph(V);
+
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int nbr1 = edges[i][0];
+            int nbr2 = edges[i][1];
+            int weight = edges[i][2];
+
+            graph[nbr1].push_back({nbr2, weight});
+            graph[nbr2].push_back({nbr1, weight});
+        }
+
+        vector<int> dist(V, INT_MAX);
+        dist[src] = 0;
+
+        priority_queue<pair<int, int>> heap; // max-heap
+        heap.push({0, src});
+
+        while (!heap.empty())
+        {
+
+            pair<int, int> topEl = heap.top();
+            heap.pop();
+
+            int currDist = -topEl.first;
+            int node = topEl.second;
+            if (currDist > dist[node])
+            {
+                continue;
+            }
+
+            for (auto &edge : graph[node])
+            {
+                int nbr = edge.first;
+                int weight = edge.second;
+                int newDist = weight + currDist;
+                if (newDist < dist[nbr])
+                {
+                    dist[nbr] = newDist;
+                    heap.push({-newDist, nbr});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+*/
 
 
